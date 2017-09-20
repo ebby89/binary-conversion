71ef75ef07dedfb2634c57ed54e94fa089522da6
Kyle Ebersole and Rane Fields <kyle@>
2017-09-20 14:47:02 -0700

    Add rest of script to enable conversion from binary to decimal.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,23 @@
 var numberArray = []
 
 var convertToDecimal = function(baseTwoNumber) {
-  for (i = 0; i < baseTwoNumber.length; i++) {
-    if (baseTwoNumber[i] !== "0" && baseTwoNumber[i] !== "1") {
+
+  var total = 0;
+  var currentPower = 1;
+
+  for (i = baseTwoNumber.length - 1; i >= 0 ; i--) {
+    var currentDigit = baseTwoNumber[i];
+
+    if (currentDigit !== "0" && currentDigit !== "1") {
       return null;
     }
+    if (currentDigit === "1") {
+      total += currentPower;
+    }
+    currentPower *= 2;
   }
 
-  return "yay!";
+  return total;
 }
 
 
